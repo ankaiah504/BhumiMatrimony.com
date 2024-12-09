cf1f50fe99ab7f006e702d72324bf576af2da16d
ankaiah504 <ankaiah.sree18@gmail.com>
2024-12-09 21:20:11 +0530

    Changes moved

--- Investor-Service/src/main/java/com/app/TestServicesApplication.java
+++ Investor-Service/src/main/java/com/app/TestServicesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.annotation.KafkaListener;
 
 import com.app.constant.AppConstant;
 import com.app.model.EmployeeDTO;
@@ -14,16 +14,16 @@ import com.app.model.EmployeeDTO;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAspectJAutoProxy(proxyTargetClass = false)
+//@EnableAspectJAutoProxy(proxyTargetClass = false)
 public class TestServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestServicesApplication.class, args);
 	}
-	@KafkaListener(topics = AppConstant.TOPIC, groupId="group_emp_details_topic")
-	public void subscribeMsg(String order) {
-		System.out.print("*** Msg Recieved From Kafka *** :: ");
-		System.out.println(order);
-		//logic
-	}
+//	@KafkaListener(topics = AppConstant.TOPIC, groupId="group_emp_details_topic")
+//	public void subscribeMsg(String order) {
+//		System.out.print("*** Msg Recieved From Kafka *** :: ");
+//		System.out.println(order);
+//		//logic
+//	}
 }
